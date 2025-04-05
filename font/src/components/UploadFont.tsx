fed4f49bc048e6182187e3a3d31053123d5f28b6
abir <62530610+Abir-Al-Arafat@users.noreply.github.com>
2025-04-05 13:57:30 +0600

    cleanup: unnecessary code

--- font/src/components/UploadFont.tsx
+++ font/src/components/UploadFont.tsx
@@ -9,18 +9,6 @@ const UploadFont = () => {
   const [fonts, setFonts] = useState<{ name: string; url: string }[]>([]);
   const [error, setError] = useState("");
 
-  // const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-  //   const uploadedFile = event.target.files?.[0];
-  //   console.log("uploadedFile", uploadedFile);
-
-  //   if (uploadedFile && uploadedFile.name.toLowerCase().endsWith(".ttf")) {
-  //     const url = URL.createObjectURL(uploadedFile);
-  //     setFonts((prevFonts) => [...prevFonts, { name: uploadedFile.name, url }]);
-  //     setError("");
-  //   } else {
-  //     setError("Only TTF files are allowed.");
-  //   }
-  // };
   const handleFileChange = async (
     event: React.ChangeEvent<HTMLInputElement>
   ) => {
@@ -57,18 +45,6 @@ const UploadFont = () => {
       setError("An error occurred during upload.");
     }
   };
-
-  // const handleDrop = (event: React.DragEvent<HTMLDivElement>) => {
-  //   event.preventDefault();
-  //   const droppedFile = event.dataTransfer.files[0];
-  //   if (droppedFile && droppedFile.name.toLowerCase().endsWith(".ttf")) {
-  //     const url = URL.createObjectURL(droppedFile);
-  //     setFonts((prevFonts) => [...prevFonts, { name: droppedFile.name, url }]);
-  //     setError("");
-  //   } else {
-  //     setError("Only TTF files are allowed.");
-  //   }
-  // };
 
   const handleDrop = async (event: React.DragEvent<HTMLDivElement>) => {
     event.preventDefault();
